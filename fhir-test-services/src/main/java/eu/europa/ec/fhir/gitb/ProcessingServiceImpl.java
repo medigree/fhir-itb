7704102603d701df967e67ad996927a073e73ec9
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2024-06-27 11:27:49 +0200

    more reverts

--- fhir-test-services/src/main/java/eu/europa/ec/fhir/gitb/ProcessingServiceImpl.java
+++ fhir-test-services/src/main/java/eu/europa/ec/fhir/gitb/ProcessingServiceImpl.java
@@ -1,13 +1,10 @@
 package eu.europa.ec.fhir.gitb;
 
-import com.gitb.core.AnyContent;
-import com.gitb.core.ValueEmbeddingEnumeration;
 import com.gitb.ps.Void;
 import com.gitb.ps.*;
 import com.gitb.tr.TestResultType;
 import eu.europa.ec.fhir.state.StateManager;
 import eu.europa.ec.fhir.utils.Utils;
-import org.imec.ivlab.ehealth.automation.PseudonymGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -41,20 +38,10 @@ public class ProcessingServiceImpl implements ProcessingService {
         String operation = processRequest.getOperation();
         if ("init".equals(operation)) {
             stateManager.recordConfiguration(utils.getRequiredString(processRequest.getInput(), "endpoint"));
-            var response = new ProcessResponse();
-            response.setReport(utils.createReport(TestResultType.SUCCESS));
-            return response;
-        } else if ("generatePseudonym".equals(operation)) {
-            String ehealthCertificate = utils.getSingleRequiredInputForName(processRequest.getInput(), "ehealthCertificate").getValue();
-            PseudonymGenerator generator = new PseudonymGenerator();
-            String pseudonym = generator.generatePseudonym(ehealthCertificate);
-            ProcessResponse response = new ProcessResponse();
-            AnyContent outputParam = utils.createAnyContentSimple("pseudonym", pseudonym, ValueEmbeddingEnumeration.BASE_64);
-            response.getOutput().add(outputParam);
-            return response;
-        } else {
-            throw new IllegalArgumentException("Unsupported operation: " + operation);
         }
+        var response = new ProcessResponse();
+        response.setReport(utils.createReport(TestResultType.SUCCESS));
+        return response;
     }
 
     @Override
